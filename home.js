88ac9ad87af029ee7d376921c5be58d70b1fb338
el hrychy niama <niamaelhry@gmail.com>
2023-10-24 20:48:20 +0100

    Responsive de page 1 et ajouter en panier

--- home.js
+++ home.js
@@ -1,3 +1,38 @@
+
+function myFunction() {
+    const Panier = document.getElementById("produitQuiAjouter");
+
+    Panier.insertAdjacentHTML("afterend" ,
+     `<article id="artct2Page4">
+    <div id="gauche">
+        <div id="dvImage1">
+            <img src="Rectangle.png" alt="" srcset="">
+        </div>
+        <div>
+            <h3>Product Name</h3>
+            
+           <h3>$ 1900</h3>
+           </div>
+        </div>
+
+        <div id="droit">
+        <div id="del">
+            <img src="icons8-j'aime-ça-24.png" alt="" srcset="">
+            <img src="icons8-supprimer-24.png" alt="" srcset="">
+        </div>
+        <div class="plusmoin">
+            <p class="moin">-</p>
+            <p class="Num">1</p>
+            <p class="plus">+</p>
+        </div>
+    </div>
+ </article>
+`
+    );
+  }
+
+
+
 var increment = 0;
 
 // Get all "moin" and "plus" elements
@@ -34,14 +69,36 @@ function listeafficher()
     var liste = document.getElementById("liste");
     if(liste.style.display == 'none'){
         liste.style.display = "block"
+        for(i=0 ; i<increment ; i++){
+            myFunction()
+        }   
+    }
+    else
+    liste.style.display = "none";
+}
+
+
+function filterafficher()
+{
+    var liste = document.getElementById("tableu");
+    if(liste.style.display == 'none'){
+        liste.style.display = "block"
+
+       
     }
     else
     liste.style.display = "none";
 }
 //fonction d'incrementation
+
 function Incrementatation()
 {
     increment ++;
     document.getElementById("NrProduct").innerHTML= increment;
 
 }
+ 
+
+
+
+
